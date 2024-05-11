0ac3d3f7d0692b9f5c737b6b6a68ea80bdfb1099
SudeepGowda55 <ssudeepgowda55@gmail.com>
2024-05-11 16:30:03 +0530

    notification code updated

--- notification_service/server.js
+++ notification_service/server.js
@@ -2,18 +2,17 @@ const amqp = require("amqplib");
 
 require("dotenv").config()
 
+function sendMail(message){
+    console.log(" [Notification Service] Received the message", message.content.toString())
+}
+
 async function connectToRabbitMQ() {
     try {
         const connection = await amqp.connect(process.env.RABBITMQ_URL);
         const channel = await connection.createChannel();
         await channel.assertQueue("file_converted_notification", { durable: false });
         console.log(" [Notification Service] waiting for messages from the file_converted_notification queue");
-
-        channel.consume("file_converted_notification", function(msg) {
-            console.log(" [Notification Service] Received the message", msg.content.toString())
-        }, {
-            noAck: true
-        });
+        channel.consume("file_converted_notification", function(msg) {sendMail(msg)}, {noAck: true});
     } catch (error) {
         console.error(error)
     }
