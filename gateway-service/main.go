c258a48d02bb6520d07159d8e2fa6747a5be088b
SudeepGowda55 <ssudeepgowda55@gmail.com>
2024-06-09 15:35:27 +0530

    header added

--- gateway-service/main.go
+++ gateway-service/main.go
@@ -13,7 +13,7 @@ func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, usermail")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
